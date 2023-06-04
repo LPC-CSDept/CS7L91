65a4fe0ff0806e034a9f0adfc50682f5821ce553
Kyu Lee <kwooler@gmail.com>
2023-06-04 13:14:42 -0700

    L91

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 
 
-def makeStudent(student):
-    ##################################################
-    # Code your program here
-    ##################################################
+def makeStudent(names, scores):
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+
+    return student
 
 
 def printStudent(student):
-    ##################################################
-    # Code your program here
-    ##################################################
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+    for k, v in student.items():
+        print(f'{k} : \t {v}')
 
 
 def getMaxScore(student):
-    ##################################################
-    # Code your program here
-    ##################################################
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
 
 
 def main():
-    student = {}
-    makeStudent(student)
+    names = ['Kim', 'Bill', 'Mary']
+    scores = [[100, 80, 70, 60],
+              [100, 90, 80, 60],
+              [90, 80, 70, 100]]
+    student = makeStudent(names, scores)
     printStudent(student)
     sname = getMaxScore(student)
     print(f'The max score student {sname} : {student[sname]}')
--- main_test.py
+++ main_test.py
@@ -9,19 +9,25 @@ import types
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = 'Kim \n 100 80 70 60\n Bill \n 100 90 80 60 \n Mary \n 90 80 70 100'
-    sys.stdin = io.StringIO(datastr)
+    # datastr = 'Kim \n 100 80 70 60\n Bill \n 100 90 80 60 \n Mary \n 90 80 70 100'
+    # sys.stdin = io.StringIO(datastr)
 
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    student = {}
-    main.makeStudent(student)
+    names = ['Kim', 'Bill', 'Mary']
+    scores = [[100, 80, 70, 60],
+              [100, 90, 80, 60],
+              [90, 80, 70, 100]]
+    student = main.makeStudent(names, scores)
+    print(f'Your return value is : {student}')
+    assert isinstance(student, dict)
     main.printStudent(student)
 
     sname = main.getMaxScore(student)
+    assert sname == 'Mary'
     assert student[sname] == [90, 80, 70, 100]
     # regex_string = r'[\w,\W]*1'
     # regex_string += r'[\w,\W]*3'
@@ -31,3 +37,28 @@ def test_main_1():
     # res = re.search(regex_string, main.evenlist)
     # assert res != None
     # print(res.group())
+
+
+def test_main_2():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    # datastr = 'Kim \n 100 80 70 60\n Bill \n 100 90 80 60 \n Mary \n 90 80 70 100'
+    # sys.stdin = io.StringIO(datastr)
+
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    names = ['Jane', 'John', 'Tom']
+    scores = [[100, 100, 100],
+              [90, 90, 90],
+              [80, 80, 80]]
+    student = main.makeStudent(names, scores)
+    print(f'Your return value is : {student}')
+    assert isinstance(student, dict)
+    main.printStudent(student)
+
+    sname = main.getMaxScore(student)
+    assert sname == 'Jane'
+    assert student[sname] == [100, 100, 100]
